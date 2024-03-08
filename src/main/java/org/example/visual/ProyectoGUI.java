306bc16c5d4f877deaa00f27394d68b899e0cc5b
Adan Ortiz <adanortiz9876@gmail.com>
2024-03-07 21:07:12 -0600

    Version 1 Creacion de packages Prototipo de interfaz de usuario

---
+++ src/main/java/org/example/visual/ProyectoGUI.java
@@ -0,0 +1,23 @@
+package org.example.visual;
+
+import javax.swing.*;
+
+public class ProyectoGUI extends JFrame {
+
+
+    private JPanel panelGeneral;
+    private JPanel panelNorte;
+    private JPanel panelCentro;
+    private JPanel panelSur;
+    private JButton btnHome;
+    private JButton btnRegistro;
+    private JButton btnBiblioteca;
+    private JPanel panelScroll;
+    private JPanel panelContenido;
+    private JLabel lblCanciones;
+    private JScrollPane scrlCanciones;
+    private JPanel panelCanciones;
+    private JScrollPane scrollContenido;
+    private JScrollPane scrollArtistas;
+
+}
